92c21a87d3f5407b4308e2d4a7fea5dd669c5af3
I337593 <kevin.jin01@sap.com>
2019-07-15 22:20:59 +0800

    Register service in euraka

--- src/main/java/com/example/waiterservice/WaiterServiceApplication.java
+++ src/main/java/com/example/waiterservice/WaiterServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -19,6 +20,7 @@ import java.util.TimeZone;
 @SpringBootApplication
 @EnableCaching
 @EnableJpaRepositories
+@EnableDiscoveryClient
 public class WaiterServiceApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
